db30351014440d0290304355a20a668180b182b7
Thomas Guillet <guillet.thomas@gmail.com>
2018-08-28 16:26:19 +0200

    WIP

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,83 @@ class Index extends React.Component {
   constructor(props) {
     super(props)
 
+    this.fetchSource = this.fetchSource.bind(this);
+    this.fetchTrace = this.fetchTrace.bind(this);
     this.handleHostChange = this.handleHostChange.bind(this);
     this.handleRootCalculationChange = this.handleRootCalculationChange.bind(this);
+    this.handleSourceChange = this.handleSourceChange.bind(this);
     this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
     this.hasTrace = this.hasTrace.bind(this);
 
     this.state = {
-      host: 'https://fr.openfisca.org/api/v21',//'http://127.0.0.1:2000',
-      situation: JSON.stringify(input, null, 2),
+      host: 'http://127.0.0.1:2000',
+      source: 'http://localhost:9000/api/situations/58bd260d4aa60a680a741e68/openfisca-request',
       result: '',
       root: ''
     }
+  }
+
+  componentDidMount() {
+    if (! window.document.location.search) {
+      return
+    }
+
+    const query = window.document.location.search.substring(1)
+    const params = query.split('&').reduce((params, tuple) => {
+      const pair = tuple.split('=')
+
+      params[pair[0]] = decodeURI(pair[1])
+      return params
+    }, {})
+
+    this.setState({
+      host: params.host,
+      source: params.source,
+    }, this.fetchSource)
+  }
+
+  componentDidUpdate(previousState, newState) {
+    if (previousState.source != newState.source) {
+      //this.fetchSource();
+    }
+  }
+
+  fetchSource() {
+    console.log(this.state.source)
+    return fetch(this.state.source)
+      .then(result => {
+        return result.json()
+      })
+      .then(result => {
+        result && this.setState({ situation: JSON.stringify(result, null, 2) }, this.fetchTrace)
+      })
+      .catch(error => {
+        this.setState({ error })
+      })
+  }
+
+  fetchTrace() {
+    console.log("fetchTrace")
+    fetch(`${this.state.host}/trace`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: this.state.situation
+      })
+      .then(result => result.json())
+      .then(result => {
+        result.requestedCalculations.sort()
+        return result
+      })
+      .then(result => result && this.setState({ result: result }))
+      .then(() => {
+        if (! this.hasTrace())
+          return
+
+        this.setState({ root: this.state.result.requestedCalculations[0] })
+      })
   }
 
   hasTrace() {
@@ -30,28 +96,16 @@ class Index extends React.Component {
     this.setState({ host: event.target.value })
   }
 
+  handleSourceChange(event) {
+    this.setState({ source: event.target.value })
+  }
 
   handleTextAreaChange(event) {
     this.setState({ situation: event.target.value })
 
      this.setState({ result: '', root: '' })
 
-    fetch(`${this.state.host}/trace`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: event.target.value
-      })
-      .then(result => result.json())
-      .then(result => result && this.setState({ result: result }))
-      .then(() => {
-        if (! this.hasTrace())
-          return
-
-        this.setState({ root: this.state.result.requestedCalculations[0] })
-      })
+     this.fetchTrace();
   }
 
   handleRootCalculationChange(event) {
@@ -61,21 +115,34 @@ class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <section>
+
+        <style jsx>{`
+          textarea {
+            min-height: 20rem;
+            resize: vertical;
+          }
+
+          .error {
+            background-color: #ec7676;
+          }
+        `}</style>
+        <section className={ this.state.error ? 'error' : ''}>
           <div className="container">
             <h2 className="section__title">OpenFisca Tracer</h2>
             <p className="section__subtitle">A tool to investigate OpenFisca computations</p>
 
-            <style jsx>{`
-              textarea {
-                min-height: 20rem;
-                resize: vertical;
-              }
-            `}</style>
+            { (this.state.error) && (
+              <h3>{ this.state.error.toString() }</h3>
+            )}
+
             <div>
               <div className="form__group">
                 <label htmlFor="host">OpenFisca base URL</label>
                 <input id="host" value={this.state.host} onChange={this.handleHostChange}/>
+              </div>
+              <div className="form__group">
+                <label htmlFor="source">OpenFisca request source URL</label>
+                <input id="source" value={this.state.source} onChange={this.handleSourceChange}/>
               </div>
               <div className="form__group">
                 <label htmlFor="request">Request content</label>
