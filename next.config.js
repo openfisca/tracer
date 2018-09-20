54ecfce3448d05a2c3af97d9445e66d271265c8c
Alexandre Segura <mex.zktk@gmail.com>
2018-09-20 11:21:16 +0200

    Fix assetPrefix.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
 module.exports = withCSS(withImages({
-  assetPrefix: isProduction ? '/{reponame}' : '',
+  assetPrefix: isProduction ? '/openfisca-tracer' : '',
   exportPathMap: function(defaultPathMap) {
     return {
       '/': { page: '/' }
